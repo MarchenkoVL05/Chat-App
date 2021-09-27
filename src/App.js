b15a0993d17a7ac1b02effffdf5433793bb56e7c
Vladimir <Shamoointhedark@gmail.com>
2021-09-28 02:27:41 +0300

    changed socket in app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './index.css';
 import io from 'socket.io-client';
 import Chat from './Chat';
 
-const socket = io.connect('http://localhost:3001');
+const socket = io.connect('https://let-is-chat.herokuapp.com/');
 
 function App() {
 
